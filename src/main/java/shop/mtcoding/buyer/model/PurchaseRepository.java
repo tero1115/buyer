77ed9c5fd6cdd203fd4c8cd7785066d84e258db3
wltkd22 <wltkd22@naver.com>
2023-01-17 17:10:12 +0900

    레파지토리 설계 완료2

---
+++ src/main/java/shop/mtcoding/buyer/model/PurchaseRepository.java
@@ -0,0 +1,18 @@
+package shop.mtcoding.buyer.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PurchaseRepository {
+    public int insert(int id, int userId, int productId);
+
+    public List<Purchase> findAll();
+
+    public Purchase findById(int id);
+
+    public int updateById(int id, int userId, int productId);
+
+    public int deleteById(int id);
+}
